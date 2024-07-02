1ac4d9ed39f682d06ed0df4f46b589de8476f837
developerjannatmim <mim266758@gmail.com>
2024-07-02 22:56:04 +0600

    - create footer component - added footer component in App

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Outlet } from 'react-router-dom'
 import NavItems from './components/NavItems'
+import Footer from './components/Footer'
 
 function App() {
   return (
     <>
       <NavItems/>
       <Outlet/>
+      <Footer/>
     </>
   )
 }
