f0f9a682c38d56c033b9a2cd98ce40c6adf97d7c
developerjannatmim <mim266758@gmail.com>
2024-07-02 12:51:55 +0600

    - added blog page - added ratting component - added selected category component - added home category component

---
+++ src/home/Banner.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import ProductData from "../products.json";
+import SelectedCategory from "../components/SelectedCategory";
+
+const title = <h2>Search Your One From <span>Thousand</span> Of Products</h2>;
+const desc = "We have the largest collection of products";
+
+const bannerList = [
+	{
+		iconName: "icofont-users-alt-4",
+		text: "1.5 Million Customers",
+	},
+	{
+		iconName: "icofont-notification",
+		text: "More then 2000 Marchent",
+	},
+	{
+		iconName: "icofont-globe",
+		text: "Buy Anything Online",
+	},
+];
+
+const Banner = () => {
+	const [searchInput, setSearchInput] = useState("");
+	const [filterProducts, setFilterProducts] = useState(ProductData);
+	// console.log(ProductData)
+
+	const handleSearch = (e) => {
+		const searchTerm = e.target.value;
+		setSearchInput(searchTerm)
+
+		// filtering products based on search
+		const filtered = ProductData.filter((product) => product.name.toLowerCase().includes(searchTerm.toLowerCase()));
+		setFilterProducts(filtered);
+	}
+
+  return <div className="banner-section style-4">
+		<div className="container">
+			<div className="banner-content">
+				{title}
+				<form>
+					<SelectedCategory select={"all"}/>
+					<input type="text" name="search" id="search" placeholder="Search your products" value={searchInput} onChange={handleSearch}/>
+					<button type="submit">
+						<i className="icofont-search"></i>
+					</button>
+				</form>
+				<p>{desc}</p>
+				<ul className="lab-ul">
+					{
+						searchInput && filterProducts.map((product, i) => <li key={i}>
+							<Link to={`/shop/${product.id}`}>{product.name}</Link>
+						</li>)
+					}
+				</ul>
+			</div>
+		</div>
+	</div>;
+};
+
+export default Banner;
